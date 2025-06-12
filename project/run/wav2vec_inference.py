749d14b0f631208d6e749814cb26c0dba9924ffa
sehoonk - ku <sehoon787@naver.com>
2025-06-12 11:39:44 +0900

    init proj

--- project/run/wav2vec_inference.py
+++ project/run/wav2vec_inference.py
@@ -13,15 +13,15 @@ from transformers import pipeline
 # Custom imports
 import sample_util
 
-db_top_dir = "C:\\Users\\Administrator\\Desktop\\ku\\1-2\\XAI604_2025_spring\\"
-test_top_dir = os.path.join(db_top_dir, "stop_music\\music_test0")
+db_top_dir = "D:/ku/1-2/XAI604_2025_spring/"
+test_top_dir = os.path.join(db_top_dir, "stop_music/music_test0")
 
 test_dataset = sample_util.make_dataset(test_top_dir)
 
 transcriber = pipeline(
     "automatic-speech-recognition",
     model=
-    "C:\\Users\\Administrator\\Desktop\\ku\\1-2\\XAI604_2025_spring\\project\\models\\checkpoint-5000"
+    "D:/ku/1-2/XAI604_2025_spring/project/models/checkpoint-10000"
 )
 
 for data in test_dataset:
